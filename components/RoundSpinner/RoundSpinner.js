aab58bd9059c63ed8fa92bf8cdd9b809252e8611
Sergey Tyurikov <project-present@yandex.ru>
2023-05-14 00:05:30 +0700

    Исправление багов в компоненте RoundSpinner.js

--- components/RoundSpinner/RoundSpinner.js
+++ components/RoundSpinner/RoundSpinner.js
@@ -1,8 +1,11 @@
-import {renderBlock} from "../../serviceFunctions.js";
 import {RoundSpinnerCSS} from "./RoundSpinnerCSS.js";
 import {RoundSpinnerHTML} from "./RoundSpinnerHTML.js";
 
 export const RoundSpinner = (colors) => {
+
+    function renderBlock (element, template, place = 'beforeend') {
+        element.insertAdjacentHTML(place, template)
+    }
 
     renderBlock(document.body.querySelector('.training-styles'), RoundSpinnerCSS(colors) || '');
 
--- components/RoundSpinner/RoundSpinnerCSS.js
+++ components/RoundSpinner/RoundSpinnerCSS.js
@@ -9,12 +9,6 @@ export const RoundSpinnerCSS = ([color1, color2, color3]) => {
         `
         <style class="RoundSpinnerCSS">
         
-            /*.spin-wrapper {*/
-            /*  position: relative;*/
-            /*  width: 100%;*/
-            /*  height: 100vh;*/
-            /*  background: #080705;*/
-            /*}*/
             .spin-wrapper {
                 padding: ${size};
                 width: 100%;
@@ -22,53 +16,49 @@ export const RoundSpinnerCSS = ([color1, color2, color3]) => {
                 display: flex;
                 justify-content: center;
                 align-items: center;
-                
                 position: absolute;
                 background-color: #fff;
                 z-index: 2;
             }
             .spin-wrapper .spinner {
-              /*position: absolute;*/
-              height: ${size};
-              width: ${size};
-              border: 3px solid transparent;
-              border-top-color: ${color2};
-              /*top: 50%;*/
-              /*left: 50%;*/
-              margin: -30px;
-              border-radius: 50%;
-              animation: spin 2s linear infinite;
+                  height: ${size};
+                  width: ${size};
+                  border: 3px solid transparent;
+                  border-top-color: ${color2};
+                  margin: -30px;
+                  border-radius: 50%;
+                  animation: spin 2s linear infinite;
             }
             .spin-wrapper .spinner:before, .spin-wrapper .spinner:after {
-              content: "";
-              position: absolute;
-              border: 3px solid transparent;
-              border-radius: 50%;
+                  content: "";
+                  position: absolute;
+                  border: 3px solid transparent;
+                  border-radius: 50%;
             }
             .spin-wrapper .spinner:before {
-              border-top-color: ${color1};
-              top: -12px;
-              left: -12px;
-              right: -12px;
-              bottom: -12px;
-              animation: spin 3s linear infinite;
+                  border-top-color: ${color1};
+                  top: -12px;
+                  left: -12px;
+                  right: -12px;
+                  bottom: -12px;
+                  animation: spin 3s linear infinite;
             }
             .spin-wrapper .spinner:after {
-              border-top-color: ${color3};
-              top: 6px;
-              left: 6px;
-              right: 6px;
-              bottom: 6px;
-              animation: spin 4s linear infinite;
+                  border-top-color: ${color3};
+                  top: 6px;
+                  left: 6px;
+                  right: 6px;
+                  bottom: 6px;
+                  animation: spin 4s linear infinite;
             }
             
             @keyframes spin {
-              0% {
-                transform: rotate(0deg);
-              }
-              100% {
-                transform: rotate(360deg);
-              }
+                  0% {
+                    transform: rotate(0deg);
+                  }
+                  100% {
+                    transform: rotate(360deg);
+                  }
             }
         </style>
         `
